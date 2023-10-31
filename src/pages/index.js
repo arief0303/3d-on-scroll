9310bb1b605882fe73eea0336002a79dd62f78c3
Arief R Syauqie <arief0303@gmail.com>
2023-10-31 20:16:30 +0700

    tweenjs rnd

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { useRef, useEffect, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { GlobalCanvas, ScrollScene, UseCanvas, SmoothScrollbar } from '@14islands/r3f-scroll-rig';
 import { MeshDistortMaterial, GradientTexture } from '@react-three/drei';
+import * as TWEEN from '@tweenjs/tween.js'
 
 function ExampleComponent() {
   const el = useRef()
@@ -43,8 +44,39 @@ function SpinningBoxSection() {
 
 function SpinningBoxWebGL({ scale, scrollState }) {
   const mesh = useRef()
+  const coords = { x: 0, y: 0 } // Start at (0, 0)  
+
+  const tween = new TWEEN.Tween(coords, false) // Create a new tween that modifies 'coords'.
+    .to({ x: 2, y: -1.5 }, 1000)
+    .easing(TWEEN.Easing.Quadratic.InOut) // Use an easing function to make the animation smooth.
+    .onUpdate(() => {
+      // Called after tween.js updates 'coords'.
+      // Move 'box' to the position described by 'coords' with a CSS translation.
+      // box.style.setProperty('transform', 'translate(' + coords.x  + coords.y)
+      mesh.current.position.x = coords.x
+      mesh.current.position.y = coords.y
+    })
+    // .start() // Start the tween immediately.
+
   useFrame(() => {
-    mesh.current.rotation.y = scrollState.progress * Math.PI * 2
+    // mesh.current.position.y = scrollState.progress * -1
+    // console.log(scrollState.progress)
+    // if scroll state is 0.5, position.y should be 0
+    if (scrollState.progress < 0.5) {
+      // mesh.current.position.y = 
+      // mesh.current.position.x = coords.x
+      // mesh.current.position.y = coords.y
+    } else {
+      // mesh.current.position.y = scrollState.progress * -1
+      //add tweenjs for animating to new position
+      // mesh.current.position.x = 2
+      // mesh.current.position.y = -1.5
+      //update mesh position with tweenjs
+      tween.start();
+    }
+    tween.update()
+    // mesh.current.position.y = scrollState.progress * -1
+
   })
   return (
     <group scale={scale.xy.min() * 0.5}>
@@ -67,7 +99,7 @@ const IndexPage = () => {
       <SmoothScrollbar>
         {(bind) => (
           <article {...bind}>
-            <header>
+            <header className="fixed top-0 left-0 z-50 p-0">
               <a href="https://github.com/14islands/r3f-scroll-rig">@14islands/r3f-scroll-rig</a>
             </header>
             <section>
@@ -81,9 +113,19 @@ const IndexPage = () => {
                 </p>
               </section>
             )}
-            <ExampleComponent />
-            <section>Both these ScrollScenes are tracking DOM elements and scaling their WebGL meshes to fit.</section>
             <SpinningBoxSection />
+            <section>Both these ScrollScenes are tracking DOM elements and scaling their WebGL meshes to fit.</section>
+            {/* <ExampleComponent /> */}
+            <section>
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+            </section>
           </article>
         )}
       </SmoothScrollbar>
